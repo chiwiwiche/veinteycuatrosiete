3c2e4cc0b239911c95ca8341361039e972da5bac
chiwiwiche <chiwiwiche@gmail.com>
2022-07-30 07:09:08 -0600

    web page version 2

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,3 +1,20 @@
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
+    
+
+
+// function myFunctions() {
+//     if (burger.addEventListener('click', funt)) {
+//         nav.classList.toggle('nav-active');
+//     } else {
+        
+//     }
+// }
+
+// myFunctions();
+
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -8,6 +25,7 @@ const navSlide = () => {
     burger.addEventListener('click', () => {
         //Toggle nav
         nav.classList.toggle('nav-active');
+        
 
 
         //Animate links
@@ -15,16 +33,22 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
                 
+                
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                
             }
         });
 
         burger.classList.toggle('toggle');
+        
 
     });
+
+    
     
     
 }
+
 
 navSlide();
