aa4027690926d1f28010213138b711e6b89ff7a4
chiwiwiche <chiwiwiche@gmail.com>
2022-08-01 15:55:24 -0600

    NavBar Error

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -19,6 +19,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+
     
 
 
@@ -27,6 +28,27 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
         
 
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+                
+                
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                
+            }
+        });
+
+        burger.classList.toggle('toggle');
+        
+
+    });
+
+    nav.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
 
         //Animate links
         navLinks.forEach((link, index) => {
